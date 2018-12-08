a236744625bd2724d2eb8d374edc7fb08b54f3d3
Issei Takaesu <e185759@ie.u-ryukyu.ac.jp>
2018-12-09 01:16:10 +0900

    report

--- src/main/jp/ac/uryukyu/ie/e185759/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e185759/Enemy.java
@@ -8,11 +8,7 @@ package jp.ac.uryukyu.ie.e185759;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Enemy {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
+public class Enemy extends LivingThing{
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -21,25 +17,8 @@ public class Enemy {
      * @param attack モンスターの攻撃力
      */
     public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        super(name, maximumHP, attack);
     }
-
-    /**
-     * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
-     * @return boolean
-     */
-    public boolean isDead() {
-        return dead;
-    }
-
-    public String getName(){
-        return name;
-    }
-
 
 
     /**
@@ -48,10 +27,10 @@ public class Enemy {
      * @param damage 受けたダメージ
      */
     public void wounded(int damage){
-        hitPoint -= damage;
-        if( hitPoint < 0 ) {
-            dead = true;
-            System.out.printf("モンスター%sは倒れた。\n", name);
+        damageVal(damage);
+        if(damageVal(damage) < 0 ) {
+            setDead(true);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
         }
     }
 
--- src/test/jp/ac/uryukyu/ie/e185759/EnemyTest2.java
+++ src/test/jp/ac/uryukyu/ie/e185759/EnemyTest2.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class EnemyTest {
+public class EnemyTest2 {
     /**
      * 「死んでる状態では攻撃できない」ことを確認。
      */
