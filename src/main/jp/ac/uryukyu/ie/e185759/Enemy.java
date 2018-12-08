c4e9d8e452223a4966929b0364c3ae1c400ad8d7
Issei Takaesu <e185759@ie.u-ryukyu.ac.jp>
2018-12-09 00:56:08 +0900

    report

--- src/main/jp/ac/uryukyu/ie/e185759/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e185759/Enemy.java
@@ -40,22 +40,7 @@ public class Enemy {
         return name;
     }
 
-    /**
-     * Heroへ攻撃するメソッド。
-     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-     * @param hero 攻撃対象
-     */
-    public void attack(Hero hero){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
-        if(dead == false){
-            int damage = (int)(Math.random()*attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-            hero.wounded(damage);
-        }
 
-    }
 
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
